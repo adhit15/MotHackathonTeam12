6eff05abee79cc49deab568d0484530ab276315b
vkaly <vkaly@Kalyan>
2019-08-10 12:26:33 +0530

    Modified and renamed the class from Queue to QueueModel

--- messagingqueue/src/main/java/com/dbs/hacktron/model/Queue.java
+++
@@ -1,7 +0,0 @@
-package com.dbs.hacktron.model;
-
-public class Queue {
-	public int id;
-	public String name;
-	public int limit;
-}
---
+++ messagingqueue/src/main/java/com/dbs/hacktron/model/QueueModel.java
@@ -0,0 +1,58 @@
+package com.dbs.hacktron.model;
+
+import java.util.Queue;
+
+public class QueueModel {
+
+	long queueId;
+	String queueName;
+	long maxLimit;
+	Queue<String> message;
+
+	public QueueModel(long queueId, String queueName, long maxLimit, Queue<String> message) {
+		super();
+		this.queueId = queueId;
+		this.queueName = queueName;
+		this.maxLimit = maxLimit;
+		this.message = message;
+	}
+
+	public long getQueueId() {
+		return queueId;
+	}
+
+	public void setQueueId(long queueId) {
+		this.queueId = queueId;
+	}
+
+	public String getQueueName() {
+		return queueName;
+	}
+
+	public void setQueueName(String queueName) {
+		this.queueName = queueName;
+	}
+
+	public long getMaxLimit() {
+		return maxLimit;
+	}
+
+	public void setMaxLimit(long maxLimit) {
+		this.maxLimit = maxLimit;
+	}
+
+	public Queue<String> getMessage() {
+		return message;
+	}
+
+	public void setMessage(Queue<String> message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "QueueModel [queueId=" + queueId + ", queueName=" + queueName + ", maxLimit=" + maxLimit + ", message="
+				+ message + "]";
+	}
+
+}
