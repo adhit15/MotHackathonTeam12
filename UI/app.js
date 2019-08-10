4869a98ab1ef936fe936ed58e72fb708b6a64c67
sairam <sairam.kesetty@gmail.com>
2019-08-10 04:36:37 -0700

    Final UI changes

--- UI/app.js
+++ UI/app.js
@@ -1,39 +1,35 @@
-var app = angular.module('app', ['ngTouch', 'ui.grid', 'ui.grid.selection','ui.grid.grouping']);
+var app = angular.module('app', []);
 
-app.controller('MainCtrl', ['$scope', '$http', '$log', '$timeout', 'uiGridConstants', 'uiGridGroupingConstants','$interval', function ($scope, $http, $log, $timeout, uiGridConstants, uiGridGroupingConstants,$interval) {
-  $scope.gridOptions = {
-    enableRowSelection: true,
-    enableSelectAll: true,
-    selectionRowHeaderWidth: 35,
-    enableGroupHeaderSelection: false,
-    rowHeight: 35,
-    showGridFooter:true,
-    enableColumnResizing: true
+app.controller('MainCtrl', ['$scope', '$http', '$log', '$timeout','$window', function ($scope, $http, $log, $timeout,$window) {
+
+      $http({
+        method: 'GET',
+        url: 'http://10.10.0.72:8080/queue/all'
+     }).then(function (response){
+      $scope.queueList = response.data;
+      $scope.Length=response.data.Length;  
+     },function (error){
+  
+     });
+     $scope.postdata = function( qname, maximumsize){
+     var data = {qid:'1', qname:qname, maximumsize:maximumsize, lastModifiedDate:null,createdDate:null};
+     
+     $http.post('http://10.10.0.72:8080/queue/add', JSON.stringify(data)).then(function (response) {
+
+      if (response.data)
+      
+      $scope.msg = "Post Data Submitted Successfully!";
+      console.log($scope.msg);
+      $window.location.reload();
+    })
+    
   };
+  $scope.edit = function(queue){
+    $scope.qid= queue.qid;
+    $scope.qname= queue.qname;
+    $scope.maximumsize= queue.maximumsize;
+    
 
-  $scope.gridOptions.columnDefs = [
-    { name: 'id', width:'10%', displayName:'QueueId'},
-    { name: 'name', displayName:'QueueName' }
-  ];
-
-  $scope.gridOptions.multiSelect = true;
-
-  // $http.get('http://10.10.0.73:8080/getQueaus')
-  //   .success(function(data) {
-  //     $scope.gridOptions.data = data;
-  //     $timeout(function() {
-  //       if($scope.gridApi.selection.selectRow){
-  //         $scope.gridApi.selection.selectRow($scope.gridOptions.data[0]);
-          
-  //       }
-  //     });
-  //   });
-
-    $scope.info = {};
-
-  $http.get('http://10.10.0.73:8080/getQueaus')
-    .success(function(data) {
-      $scope.gridOptions.data = data;
-
-      });
+  
+  }
 }]);
